c89d94803edcc39d46c7a2d3ffbb12d57f4a0b26
Alan Samuel Martinez Alonso <leumas.mart26@gmail.com>
2025-09-08 17:57:07 -0600

    FEAT: called from...

--- services/routes/internal/handler/http/healthz.go
+++ services/routes/internal/handler/http/healthz.go
@@ -2,10 +2,12 @@ package http
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[routes] health called from %s", r.RemoteAddr)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprintf(w, "%s service ok", "routes")
 }
--- services/workouts/internal/handler/http/healthz.go
+++ services/workouts/internal/handler/http/healthz.go
@@ -2,9 +2,12 @@ package http
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
+
 func Healthz(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[workouts] healthz called from %s", r.RemoteAddr)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprintf(w, "%s service ok", "workouts")
-}	
+}
